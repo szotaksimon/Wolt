326941c7fec12a8bcef862404161383ad9c7c853
Levente Ibrányi <ibranyilevente@gmail.com>
2021-12-16 12:20:21 +0100

    Test commit

--- app/src/main/java/com/example/woltstat/MainActivity.java
+++ app/src/main/java/com/example/woltstat/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.woltstat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnPenzugy;
+    private Button btnTankolas;
+    private Button btnStatisztika;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+    private void init(){
+
     }
 }
