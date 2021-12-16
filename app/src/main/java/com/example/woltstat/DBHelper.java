ade147d6156ea02bff49192cd1e556a63d10f9dc
Szoták Simon <szotak.simon16@gmail.com>
2021-12-16 14:20:55 +0100

    DBHelper Kész

--- app/src/main/java/com/example/woltstat/DBHelper.java
+++ app/src/main/java/com/example/woltstat/DBHelper.java
@@ -14,10 +14,10 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final int DB_VERSION = 1;
 
     private static final String TABLE_NAME = "adatok";
-    private static final String COL_ID = "id";
-    private static final String COL_VEZNEV = "vezeteknev";
-    private static final String COL_KERNEV = "keresztnevnev";
-    private static final String COL_JEGY = "jegy";
+    private static final String COL_BRUTTO = "brutto";
+    private static final String COL_NETTO = "netto";
+    private static final String COL_MEGTETT_KM = "megtettKM";
+    private static final String COL_TANKOLAS =  "tank";
 
     public DBHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -26,10 +26,10 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String sql = "CREATE TABLE "+TABLE_NAME +" (" +
-                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COL_VEZNEV + " TEXT NOT NULL, " +
-                COL_KERNEV + " TEXT NOT NULL, " +
-                COL_JEGY + " INTEGER NOT NULL " +
+                COL_BRUTTO + " INTEGER NOT NULL, " +
+                COL_NETTO + " INTEGER NOT NULL, " +
+                COL_MEGTETT_KM + " INTEGER NOT NULL, " +
+                COL_TANKOLAS + " INTEGER NOT NULL " +
                 ");";
         db.execSQL(sql);
     }
@@ -40,37 +40,24 @@ public class DBHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean rogzites(String vezeteknev, String keresztnev, int jegy){
+    public boolean rogzites(int brutto, int netto, int megtettKM, int tank){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(COL_VEZNEV, vezeteknev);
-        values.put(COL_KERNEV, keresztnev);
-        values.put(COL_JEGY, jegy);
+        values.put(COL_BRUTTO, brutto);
+        values.put(COL_NETTO, netto);
+        values.put(COL_MEGTETT_KM, megtettKM);
+        values.put(COL_TANKOLAS, tank);
         return db.insert(TABLE_NAME, null, values) != -1;
     }
 
     public Cursor listaz() {
         SQLiteDatabase db = this.getReadableDatabase();
-        return db.query(TABLE_NAME, new String[]{COL_ID,COL_VEZNEV,COL_KERNEV,COL_JEGY},
+        return db.query(TABLE_NAME, new String[]{COL_BRUTTO,COL_NETTO,COL_MEGTETT_KM,COL_TANKOLAS},
                 null,null,null,null,null);
         /*
         db.rawQuery("SELECT * FROM "+TABLE_NAME + " WHERE "+COL_VEZNEV
                 +" = ? AND "+COL_KERNEV+ " = ?", new String[]{"Gipsz", "Jakab"});
         */
 
-    }
-
-    public int torles(String id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete(TABLE_NAME, COL_ID+" = ?", new String[]{id});
-    }
-
-    public int modosit(String id, String vezeteknev, String keresztnev, String jegy){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(COL_VEZNEV, vezeteknev);
-        values.put(COL_KERNEV, keresztnev);
-        values.put(COL_JEGY, jegy);
-        return db.update(TABLE_NAME, values,COL_ID+" = ?", new String[]{id});
     }
 }
