5d652555db1f8bac5169f49e974ed1bd3360de6b
Levente Ibrányi <ibranyilevente@gmail.com>
2021-12-16 12:52:42 +0100

    Main button click functions

--- app/src/main/java/com/example/woltstat/MainActivity.java
+++ app/src/main/java/com/example/woltstat/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.woltstat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,9 +19,37 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
+        btnPenzugy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent penzugyek = new Intent(MainActivity.this, Penzugy.class);
+                startActivity(penzugyek);
+                finish();
+            }
+        });
+
+        btnTankolas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent tankolas = new Intent(MainActivity.this, Tankolas.class);
+                startActivity(tankolas);
+                finish();
+            }
+        });
+
+        btnStatisztika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent statisztika = new Intent(MainActivity.this, Statisztika.class);
+                startActivity(statisztika);
+                finish();
+            }
+        });
     }
 
     private void init(){
-
+        btnPenzugy = findViewById(R.id.btnPenzugy);
+        btnTankolas = findViewById(R.id.btnTankolas);
+        btnStatisztika = findViewById(R.id.btnStatisztika);
     }
 }
